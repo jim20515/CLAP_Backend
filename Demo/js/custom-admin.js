3f0409f5b28ecd3eacb0b7266978199e4beeb502
Jim <jim@whatsmedia.com>
2015-05-25 22:59:32 +0800

    First Version

---
+++ Demo/js/custom-admin.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $('select').selectpicker();
+
+    $('.date').datepicker({
+        language: "zh-TW",
+        clearBtn: true,
+        todayHighlight: true,
+        format: "yyyy-mm-dd",
+        autoclose: true
+    });
+
+    $('.radio :radio, .checkbox :checkbox').iCheck({
+        checkboxClass: 'icheckbox_minimal-blue',
+        radioClass: 'iradio_minimal-blue',
+        increaseArea: '20%' // optional
+    });
+});
